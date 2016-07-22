b0e2f919aceb624fee4c93601d259ae346282863
seiyamiyaoka <seiyamiyaoka@gmail.com>
2016-07-22 11:50:58 +0000

    edit registrationi controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :profile, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
