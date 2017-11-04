24401d57456d5085bd4827ec796f681074806920
伊藤淳 <itojun@itojunMacBook-Pro.local>
2017-11-04 20:18:33 +0900

    application.html.erb 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+cnt = 0
+1.times do |n|
+  email = "test@test.com"
+  uid = SecureRandom.uuid
+  password = "111111"
+  User.create!(email: email,
+              password: password,
+              password_confirmation: password,
+              name: "test" + cnt.to_s,
+              confirmed_at: Time.now
+              )
+  cnt += 1
+end
